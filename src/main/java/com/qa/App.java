d8425de7c88f3a8ee7a1afe9a4fec3efea64a0b4
gavagainagain <136992337+gavagainagain@users.noreply.github.com>
2024-09-03 12:26:24 +0100

    breaking change

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -6,7 +6,7 @@ package com.qa;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String] args )
     {
         System.out.println(sayHello());
         System.out.println(sayHelloToSomeone("Alice"));
